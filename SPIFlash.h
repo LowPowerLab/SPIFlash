e609b45a6bd505361e92600dae8a115524320940
LowPowerLab <flexiboiu@gmail.com>
2013-04-16 16:44:38 -0300

    Update SPIFlash.h

--- SPIFlash.h
+++ SPIFlash.h
@@ -59,7 +59,7 @@
 #define SPIFLASH_BYTEPAGEPROGRAM  0x02        // write (1 to 256bytes)
 #define SPIFLASH_IDREAD           0x9F        // read JEDEC manufacturer and device ID (2 bytes, specific bytes for each manufacturer and device)
                                               // Example for Atmel-Adesto 4Mbit AT25DF041A: 0x1F44 (page 27: http://www.adestotech.com/sites/default/files/datasheets/doc3668.pdf)
-                                              // Example for Winbond 4Mbit W25X40CL: 0x9F30 (page 14: http://www.winbond.com/NR/rdonlyres/6E25084C-0BFE-4B25-903D-AE10221A0929/0/W25X40CL.pdf)
+                                              // Example for Winbond 4Mbit W25X40CL: 0xEF30 (page 14: http://www.winbond.com/NR/rdonlyres/6E25084C-0BFE-4B25-903D-AE10221A0929/0/W25X40CL.pdf)
 
 class SPIFlash {
 public:
